ee4edafbcc13626d654ee98d90dabea1d55218e8
cuze <cuzekn@gmail.com>
2022-07-03 23:53:41 +0900

    chore: js -> jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>掲示板一覧</h1>
       </header>
+      <main>
+        
+      </main>
     </div>
   );
 };
